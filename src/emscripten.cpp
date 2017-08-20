24a470c1e9555b2433750af001cb6badf33c91f7
Rauli Laine <rauli.laine@iki.fi>
2017-08-20 13:32:31 +0300

    Introduce WebAssembly version of the REPL
    With the powers granted to me by the excellent Emscripten compiler, I
    can now create version of Plorth REPL that can be run inside the
    browser. Add simple run-in-the-browser Plorth environment as optional
    build target.

--- src/emscripten.cpp
+++ src/emscripten.cpp
@@ -1,26 +1,46 @@
+#include <plorth/context.hpp>
 #include <emscripten/bind.h>
 
 using namespace plorth;
 
 static memory::manager* memory_manager;
-static ref<class runtime> runtime;
-static ref<class context> context;
+static ref<runtime> script_runtime;
+static ref<context> script_context;
 
-void initialize_repl_api(const ref<class runtime>&);
+void initialize_repl_api(const ref<runtime>&);
 
+/**
+ * Returns depth of the data stack.
+ */
 static int emscripten_depth()
 {
-  return context ? context->size() : 0;
+  return script_context ? script_context->size() : 0;
 }
 
+/**
+ * Compiles and executes given source code under the REPL context.
+ */
 static void emscripten_execute(const std::string& source)
 {
-  if (!context)
+  ref<class quote> quote;
+
+  if (!script_context)
   {
     memory_manager = new memory::manager();
-    runtime = memory_manager->new_runtime();
-    context = runtime->new_context();
-    initialize_repl_api(runtime);
+    script_runtime = memory_manager->new_runtime();
+    script_context = script_runtime->new_context();
+    initialize_repl_api(script_runtime);
+  }
+
+  if ((quote = script_context->compile(source)))
+  {
+    quote->call(script_context);
+  }
+
+  if (script_context->error())
+  {
+    std::cerr << script_context->error() << std::endl;
+    script_context->clear_error();
   }
 }
 
